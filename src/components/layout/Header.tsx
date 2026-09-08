99bba6ae63cbfd0df5f84ce457ae8f0565077a09
raptur86 <cloudnil@naver.com>
2026-09-08 18:32:11 +0900

    0908-4

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,17 @@
+
+import "./Header.css"
+
+
 export function Header(){
     return (
-        <>
-            <div>견적서 작성 영역</div>
-        </>
+        <div className="flex items-center gap-1 header">
+            <div className="flex-1">통합검색</div>
+            <div>새견적</div>
+            <div className="alram">
+                <button>알림</button>
+                <span>3</span>
+            </div>
+        </div>
+        
     )
 }
